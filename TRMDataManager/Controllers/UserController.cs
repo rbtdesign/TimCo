3cb403895159f604ecd57872ae51b4344b94ea69
codingandcaffeine <codingandcaffeine@outlook.com>
2020-11-05 17:15:47 +0100

    connect db to api - get user data

---
+++ TRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    //[RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+
+        // GET: User/Id
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId(); //grabd the id from the logged in user instead of requesting the id. Otherwise you can lookup anyone
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+
+        }
+
+      
+    }
+}
